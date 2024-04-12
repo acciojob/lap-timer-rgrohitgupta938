ea8b9acb898f8f284fa0aee9b63cfbe1ae616be2
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-12 17:56:00 +0530

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,71 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [time, setTime] = useState(0);
+  const [lap, setlap] = useState([]);
+  const [running, setRunning] = useState(false);
+
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    return clearInterval(intervalRef.current);
+  }, []);
+
+  const startTimer = () => {
+    setRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 10);
+  };
+
+  const stopTimer = () => {
+    setRunning(false);
+    clearInterval(intervalRef.current);
+  };
+
+  const laps = () => {
+    if (running) {
+      setlap((prevLaps) => [...prevLaps, time]);
+    }
+  };
+
+  const resetIimer = () => {
+    setRunning(false);
+    clearInterval(intervalRef.current);
+    setTime(0);
+    setlap([]);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 6000);
+    const seconds = Math.floor((time / 100) % 60);
+    const centiseconds = time % 100;
+    return `${padNumber(minutes)}:${padNumber(seconds)}:${padNumber(
+      centiseconds
+    )}`;
+  };
+
+  const padNumber = (number) => {
+    return number.toString().padStart(2, "0");
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>{formatTime(time)}</p>
+      <div className="btn-grp">
+        <button onClick={() => startTimer()}>Start</button>
+        <button onClick={() => stopTimer()}>Stop</button>
+        <button onClick={() => laps()}>Lap</button>
+        <button onClick={() => resetIimer()}>Reset</button>
+      </div>
+      <ul>
+        {lap.map((l, index) => (
+          <li key={index}>{formatTime(l)}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
